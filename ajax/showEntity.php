31e495a62ea24c4fee0024896e4c5a2f5946bae1
aamir <aamir@debian>
2016-11-10 12:39:16 +0300

    Сортировка списков (0.9.2)

--- ajax/showEntity.php
+++ ajax/showEntity.php
@@ -2,71 +2,71 @@
 require_once 'connectParams.php';
 $mysqli = new mysqli("localhost", $dbUser, $dbPass, $dbName);
 if ($mysqli->connect_errno) {
-    printf("Ñîåäèíåíèå íå óäàëîñü: %s\n", $mysqli->connect_error);
-    exit();
+	printf("Ñîåäèíåíèå íå óäàëîñü: %s\n", $mysqli->connect_error);
+	exit();
 };
 $query = "set names utf8";
 $mysqli->query($query);
 $entity = $_POST["entity"];
 switch ($entity) {
-    case 'teacher':
+	case 'teacher':
 		showTeacher($mysqli);
-        break;
+		break;
 	case 'discipline':
 		showDiscipline($mysqli);
-        break;
-    case 'specialty':
-        showSpecialty($mysqli);
-        break;
+		break;
+	case 'specialty':
+		showSpecialty($mysqli);
+		break;
 	case 'group':
-        showGroup($mysqli);
-        break;
+		showGroup($mysqli);
+		break;
 	case 'teacherLoad':
-        showTeacherLoad($mysqli);
-        break;
+		showTeacherLoad($mysqli);
+		break;
 };
 $mysqli->close();
 
 //Ðåàëèçàöèÿ äëÿ êàæäîé ñóùíîñòè
 function showTeacher($mysqli){
-	$query = "SELECT * FROM teachers";
+	$query = "SELECT * FROM teachers order by lName";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo '<div>'.$row["lName"].' '.$row["fName"].' '.$row["mName"].'<span id ="'.$row["id"].'" onClick="delTeacher(this)"> Удалить </span></div>';
 		}
-    $result->free();
+	$result->free();
 	}
 };
 //
 function showDiscipline($mysqli){
-	$query = "SELECT * FROM discipline";
+	$query = "SELECT * FROM discipline order by fullName";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo $row["fullName"].' '.$row["shortName"].'<br/>';
 		}
-    $result->free();
+	$result->free();
 	}
 };
 //
 function showSpecialty($mysqli){
-	$query = "SELECT * FROM specialty";
+	$query = "SELECT * FROM specialty order by name";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo $row["code"].' '.$row["name"].'<br/>';
 		}
-    $result->free();
+	$result->free();
 	}
 };
 
 function showGroup($mysqli){
 	$query = "SELECT groups.name, specialty.name as 'specialty'
-				FROM groups
-				INNER JOIN specialty ON specialty.id = groups.specialtyId;";
+				FROM groups 
+				INNER JOIN specialty ON specialty.id = groups.specialtyId order by groups.name;";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo $row["name"].' '.$row["specialty"].'<br/>';
 		}
-    $result->free();
+	$result->free();
 	}
 };
 
@@ -74,12 +74,12 @@ function showTeacherLoad($mysqli){
 	$query = "SELECT teacherLoad.id, teachers.lName, teachers.fName, teachers.mName, groups.name as 'group', discipline.shortName as 'discipline'  FROM teacherLoad 
 		JOIN teachers ON teachers.id = teacherLoad.teacherId
 		JOIN groups ON groups.id = teacherLoad.groupId
-		JOIN discipline ON discipline.id = teacherLoad.disciplineId";
+		JOIN discipline ON discipline.id = teacherLoad.disciplineId order by teachers.lName";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo $row["lName"].' '.$row["fName"].' '.$row["mName"].' '.$row["group"].' '.$row["discipline"].'<br/>';
 		}
-    $result->free();
+	$result->free();
 	
 	}
 };
--- ajax/showList.php
+++ ajax/showList.php
@@ -2,32 +2,32 @@
 require_once 'connectParams.php';
 $mysqli = new mysqli("localhost", $dbUser, $dbPass, $dbName);
 if ($mysqli->connect_errno) {
-    printf("Соединение не удалось: %s\n", $mysqli->connect_error);
-    exit();
+	printf("Соединение не удалось: %s\n", $mysqli->connect_error);
+	exit();
 };
 $query = "set names utf8";
 $mysqli->query($query);
 
 $field = $_POST["field"];
 switch ($field) {
-    case 'teacher':
+   	 case 'teacher':
 		showTeacher($mysqli);
-        break;
+		break;
 	case 'discipline':
 		showDiscipline($mysqli);
-        break;
-    case 'specialty':
-        showSpecialty($mysqli);
-        break;
+		break;
+	case 'specialty':
+		showSpecialty($mysqli);
+		break;
 	case 'group':
-        showGroup($mysqli);
-        break;
+		showGroup($mysqli);
+		break;
 };
 $mysqli->close();
 
 
 function showTeacher($mysqli){
-	$query = "SELECT * FROM teachers";
+	$query = "SELECT * FROM teachers order by lName";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo '<option value="'.$row["id"].'">'.$row["lName"].' '.$row["fName"].' '.$row["mName"].'</option>';
@@ -37,7 +37,7 @@ function showTeacher($mysqli){
 };
 
 function showDiscipline($mysqli){
-	$query = "SELECT * FROM discipline";
+	$query = "SELECT * FROM discipline order by shortName";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo '<option value="'.$row["id"].'">'.$row["shortName"].'</option>';
@@ -47,7 +47,7 @@ function showDiscipline($mysqli){
 };
 
 function showSpecialty($mysqli){
-	$query = "SELECT * FROM specialty";
+	$query = "SELECT * FROM specialty order by code";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo '<option value="'.$row["id"].'">'.$row["code"].'</option>';
@@ -57,7 +57,7 @@ function showSpecialty($mysqli){
 };
 
 function showGroup($mysqli){
-	$query = "SELECT * FROM groups";
+	$query = "SELECT * FROM groups order by name";
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()) {
 			echo '<option value="'.$row["id"].'">'.$row["name"].'</option>';
